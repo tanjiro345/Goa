673d070937c0dbd6dea35d34fb316d5800966557
g27596533@gmail.com <g27596533@gmail.com>
2024-10-19 13:04:03 +0400

    homework

--- level20/homework.py
+++ level20/homework.py
@@ -1,4 +1,24 @@
-h1=int(input("enter your number"))
-print(h1 == 10)
+ #1 
+#h1=int(input("enter your number"))
+#print(h1 == 10)
 
+#2
+#h1=int(input("enter your number"))
+#print(h1 % 2==0)
 
+#3
+#h1=int(input("enter your number"))
+#print(h1 % 100==0)
+
+#4
+#number1 = float(input("შეიყვანეთ პირველი რიცხვი: "))
+#number2 = float(input("შეიყვანეთ მეორე რიცხვი: "))
+#number3 = float(input("შეიყვანეთ მესამე რიცხვი: "))
+#are_different = (number1 != number2) and (number1 != number3) and (number2 != number3)
+#print(are_different)
+
+#5
+username = input("შეიყვანეთ  სახელი: ")
+password = input("შეიყვანეთ პაროლი:")
+is_valid = (username == "გიო") and (password == "12345")
+print(is_valid)
