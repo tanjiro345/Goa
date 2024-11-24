2a8c073a24141574ed4c5fafa9d7762c86bb2ddf
g27596533@gmail.com <g27596533@gmail.com>
2024-11-24 12:25:56 +0400

    homework

--- level26/homework/homework.py
+++ level26/homework/homework.py
@@ -1,2 +1,24 @@
 for i in range(1, 11):
     print("გიო")
+
+
+answer = 42
+
+while True:
+    g1 = int(input("შეიყვანეთ რიცხვი: "))
+    if g1 == answer:
+        print("დამშვიდდით, თქვენ სწორად უპასუხეთ!")
+
+
+
+g2 = int(input("Enter your name:"))
+
+if g2 >= 100:
+    print("big number")
+else:
+    print("small number")
+
+
+
+
+
